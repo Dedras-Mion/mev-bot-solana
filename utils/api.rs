8394b51f659915660fd40b3da9c58bfc90efbd7c
Lucasle <dedrasmion@gmail.com>
2025-08-09 23:30:30 +0900

    update
    Co-authored-by: pontrue1031@gmail.com

--- utils/api.rs
+++ utils/api.rs
@@ -3,6 +3,7 @@ use crate::api::flipside::FlipsideApi;
 use crate::api::thegraph::TheGraphApi;
 
 pub async fn get_sniping_data(parsec_api: &ParsecApi, flipside_api: &FlipsideApi) -> Result<crate::models::sniping_data::SnipingData, Box<dyn std::error::Error>> {
+    
     let token_prices = parsec_api.get_token_prices().await?;
     
     let mut sniping_data = crate::models::sniping_data::SnipingData {
@@ -16,9 +17,11 @@ pub async fn get_sniping_data(parsec_api: &ParsecApi, flipside_api: &FlipsideApi
     }
     
     Ok(sniping_data)
+
 }
 
 pub async fn get_copy_trade_data(thegraph_api: &TheGraphApi, trader_accounts: &[String]) -> Result<Vec<crate::models::trade::Trade>, Box<dyn std::error::Error>> {
+   
     let mut all_trades = Vec::new();
     
     for trader_account in trader_accounts {
